07957b8e8832ce1ba0fd2448987f3155457bd980
swati <swatim9919@gmail.com>
2024-05-06 23:09:00 +0530

    Initial Commit

---
+++ MavenProj02/src/test/java/com/nt/service/AppTest.java
@@ -0,0 +1,44 @@
+package com.nt.service;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+	@Test
+    public void testWithPositives() {
+    	int a=10;
+    	int b=20;
+    	int expected =30;
+    	App app1 = new App();
+    	int actual = app1.sum(a, b);
+    	assertEquals(expected, actual);
+    }
+	
+	@Test
+	public void testWithNegatives()
+	{
+		int a=-10;
+    	int b=-20;
+    	int expected = -30;
+    	App app1 = new App();
+    	int actual = app1.sum(a, b);
+    	assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testWithMixedValues()
+	{
+		int a=10;
+    	int b=-20;
+    	int expected = -10;
+    	App app1 = new App();
+    	int actual = app1.sum(a, b);
+    	assertEquals(expected, actual);
+	}
+    
+}

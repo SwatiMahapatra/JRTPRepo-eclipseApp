6de38df0ba2db5566847544f1d63c955d6a3196f
swati <swatim9919@gmail.com>
2024-05-06 23:37:47 +0530

    Commit from TL.

--- MavenProj02/src/main/java/com/nt/service/B1.java
+++ MavenProj02/src/main/java/com/nt/service/B1.java
@@ -2,6 +2,6 @@ package com.nt.service;
 
 public class B1 {
 	public static void main(String[] args) {
-		System.out.println("Git operation from Eclipse");
+		System.out.println("Git operation from TL in Eclipse.");
 	}
 }
